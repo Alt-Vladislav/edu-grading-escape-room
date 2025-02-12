147165ec3150756080ba4c7fa3fa908a089d128d
Vladislav <vlad.4sa@gmail.com>
2025-02-13 03:10:34 +0700

    3.1. Добавлены модуль работы с API Axios и модуль работы с токеном.

---
+++ src/services/api.ts
@@ -0,0 +1,42 @@
+import axios, {AxiosInstance, AxiosError} from 'axios';
+import { toast } from 'react-toastify';
+import { StatusCodes } from 'http-status-codes';
+import { getToken } from './token';
+
+const BACKEND_URL = 'https://grading.design.htmlacademy.pro/v1/escape-room/';
+const REQUEST_TIMEOUT = 5000;
+const EnabledStatusCode: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: false,
+  [StatusCodes.NOT_FOUND]: true,
+  [StatusCodes.CONFLICT]: true,
+  [StatusCodes.INTERNAL_SERVER_ERROR]: true,
+} as const;
+
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+
+  api.interceptors.request.use((config) => {
+    const token = getToken();
+    if (token && config.headers) {
+      config.headers['x-token'] = token;
+    }
+    return config;
+  });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<{status: number; message: string}>) => {
+      if (error.status && EnabledStatusCode[error.status]) {
+        toast.warn(error.message);
+        throw error;
+      }
+    }
+  );
+
+  return api;
+};
---
+++ src/services/token.ts
@@ -0,0 +1,7 @@
+const AUTH_TOKEN_KEY_NAME = 'escape-room-token';
+
+const getToken = (): string => localStorage.getItem(AUTH_TOKEN_KEY_NAME) ?? '';
+const saveToken = (token: string): void => localStorage.setItem(AUTH_TOKEN_KEY_NAME, token);
+const dropToken = (): void => localStorage.removeItem(AUTH_TOKEN_KEY_NAME);
+
+export { getToken, saveToken, dropToken };
