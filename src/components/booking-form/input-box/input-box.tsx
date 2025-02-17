288677bb69aaf8376e50dddf593c48c8332d0b7f
Vladislav <vlad.4sa@gmail.com>
2025-02-17 21:48:50 +0700

    6.2. Подключил react-hook-form к форме бронирования.

--- src/components/booking-form/input-box/input-box.tsx
+++ src/components/booking-form/input-box/input-box.tsx
@@ -1,11 +1,18 @@
+import { useEffect } from 'react';
+import { BookingData } from '../../../store/types';
+import { UseFormRegister } from 'react-hook-form';
+import { toast } from 'react-toastify';
 import { capitalizeFirstLetter } from '../../../utils';
 
 type Type = keyof typeof InputSettings;
 
 type InputBoxProps = {
+  name: keyof BookingData;
   type: Lowercase<Type>;
-  pattern: string;
-  reference: React.RefObject<HTMLInputElement>;
+  pattern: RegExp;
+  isError: boolean;
+  errorText: string;
+  register: UseFormRegister<BookingData>;
 }
 
 const InputSettings = {
@@ -27,22 +34,31 @@ const InputSettings = {
 } as const;
 
 
-export default function InputBox({ type, pattern, reference }: InputBoxProps): JSX.Element {
+export default function InputBox({ name, type, pattern, isError, errorText, register }: InputBoxProps): JSX.Element {
   const setting = InputSettings[capitalizeFirstLetter(type) as Type];
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(setting.Title + errorText);
+    }
+  }, [isError, setting, errorText]);
 
   return (
     <div className="custom-input booking-form__input">
-      <label className="custom-input__label" htmlFor={setting.Id}>{setting.Title}</label>
+      <label
+        className="custom-input__label"
+        htmlFor={setting.Id}
+        {...isError && ({ style: { color: 'rgba(247, 44, 44, 0.67)' } })}
+      >
+        {setting.Title}{isError && errorText}
+      </label>
       <input
         type={type}
         id={setting.Id}
-        name={setting.Id}
         placeholder={setting.Placeholder}
-        required
-        pattern={pattern}
-        ref={reference}
+        {...register(name, { required: true, pattern: pattern })}
+        {...isError && ({ style: { backgroundColor: 'rgba(247, 44, 44, 0.4)' } })}
       />
     </div>
   );
 }
-
